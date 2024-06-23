3703059fd8ed134caa89c1c06715561ab30d266d
sruthiravi268 <sruthiravi268@gmail.com>
2024-06-23 21:07:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, how about youu?'
+    return 'I am good, how about u?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
